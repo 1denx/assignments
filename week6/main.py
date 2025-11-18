79fac4869f0de084b31d0ce9db517e515950fe48
Eden <1denchen.work@gmail.com>
2025-11-19 00:11:30 +0800

    feat: Implement user registration and login functionality

--- week6/main.py
+++ week6/main.py
@@ -1,34 +1,104 @@
-from fastapi import FastAPI, Form, Request
+import os
+import json
+import urllib.request as req
+import mysql.connector
+from dotenv import load_dotenv, find_dotenv
+from fastapi import FastAPI, Form, Request, Depends
+from typing import Annotated 
 from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.templating import Jinja2Templates
-import urllib.request as req
-import json
+
+# 載入 .env 變數
+load_dotenv(find_dotenv())
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="your_secret_key")
-# 設定模板資料夾
 templates = Jinja2Templates(directory="templates")
+
+def get_db_connector():
+    con = None
+    cursor = None
+    try:
+        con = mysql.connector.connect(
+            host = DB_HOST,
+            user = DB_USER,
+            password = DB_PASSWORD,
+            database = DB_NAME
+        )
+        cursor = con.cursor(dictionary=True)
+        print("資料庫連線成功")
+        yield con, cursor
+    except Exception as e:
+        print(f"資料庫連線錯誤: {e}")
+        raise e
+    finally:
+        if cursor:
+            cursor.close()
+        if con:
+            con.close()
 
 @app.get("/")
 async def home(request: Request):
+    if request.session.get("SIGNIN"):
+        return RedirectResponse(url="/member", status_code=303)
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/signup")
+async def signup(
+    request: Request,
+    name: Annotated[str, Form()],
+    email: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    db = Depends(get_db_connector)
+):
+    con, cursor = db
+    query = "SELECT * FROM member WHERE email = %s;"
+    cursor.execute(query, (email,))
+    user = cursor.fetchone()
+
+    if user:
+        return RedirectResponse(url="/ohoh?msg=重複的電子郵件", status_code=303)
+
+    insert_query = "INSERT INTO member (name, email, password) VALUES (%s, %s, %s)"
+    cursor.execute(insert_query,(name, email, password))
+
+    con.commit()
+    return RedirectResponse(url="/", status_code=303)
+    
 
 @app.post("/login")
 async def login(
     request: Request,
-    email: str = Form(...),
-    password: str = Form(...)
-    ):
+    email: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    db = Depends(get_db_connector)
+):
+    con, cursor = db
+
+    email = email.strip()
+    password = password.strip()
     if not email or not password:
-        msg = "請輸入信箱和密碼"
-        return RedirectResponse(url=f"/ohoh?msg={msg}", status_code=303)
-    if email == "abc@abc.com" and password == "abc":
-        request.session["login"] = True
-        return RedirectResponse(url="/member", status_code=303)
-    msg="帳號或密碼輸入錯誤"
-    return RedirectResponse(url=f"/ohoh?msg={msg}", status_code=303)
+        return RedirectResponse(url="/ohoh?msg=請輸入信箱或密碼", status_code=303)
+
+    query = "SELECT id, password FROM member WHERE email =%s;"
+    cursor.execute(query,(email,))
+    user = cursor.fetchone()
+    
+    if user is None:
+        return RedirectResponse(url="/ohoh?msg=電子郵件或密碼錯誤", status_code=303)
+    else:
+        if user["password"] == password:
+            request.session["user_id"] = user["id"]
+            return RedirectResponse(url="/member", status_code=303)
+        else:
+            return RedirectResponse(url="ohoh?msg=電子郵件或密碼錯誤", status_code=303)
 
 @app.get("/logout")
 async def logout(request: Request):
@@ -36,9 +106,10 @@ async def logout(request: Request):
     return RedirectResponse(url="/", status_code=303)
 
 @app.get("/member")
-async def member(request: Request):
-    if not request.session.get("login"):
-        return RedirectResponse(url="/")
+async def member(request: Request, db = Depends(get_db_connector)):
+    user_id = request.session.get("user_id")
+    if not user_id:
+        return RedirectResponse(url="/ohoh?msg=請先登入", status_code=303)
     return templates.TemplateResponse("member.html", {
         "request": request,
         "message": "恭喜你，成功登入系統"
